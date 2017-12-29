fb531c9de5ffb97198f3e6a11891e8f3c7590083
Gaolbirdy <gaolbird@yeah.net>
2017-12-29 21:12:30 +0800

    commit

--- RUNOOB/错误处理/错误处理.c
+++ RUNOOB/错误处理/错误处理.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 extern int errno;
 
-void main()
+//void main()
+//{
+//	FILE *pf;
+//	int errnum;
+//	pf = fopen("unexist.txt", "rb");
+//	if (pf == NULL)
+//	{
+//		errnum = errno;
+//		fprintf(stderr, "错误号：%d\n", errno);
+//		perror("通过 perror 输出错误");
+//		fprintf(stderr, "打开文件错误：%s\n", strerror(errnum));
+//	}
+//	else
+//	{
+//		fclose(pf);
+//	}
+//
+//	system("pause");
+//}
+
+main()
 {
-	FILE *pf;
-	int errnum;
-	pf = fopen("unexist.txt", "rb");
-	if (pf == NULL)
+	int dividend = 20;
+	int divisor = 5;
+	int quotient;
+
+	if (divisor == 0)
 	{
-		errnum = errno;
-		fprintf(stderr, "错误号：%d\n", errno);
-		perror("通过 perror 输出错误");
-		fprintf(stderr, "打开文件错误：%s\n", strerror(errnum));
-	}
-	else
-	{
-		fclose(pf);
+		fprintf(stderr, "除数为0 退出运行...\n");
+		//fprintf(stderr, "除数为0 退出运行...%s\n", strerror(errno));
+
+		system("pause");
+		exit(EXIT_FAILURE);
 	}
 
+	quotient = dividend / divisor;
+	//fprintf(stderr, "quotient 变量的值为：%d\n", quotient);
+	fprintf(stderr, "quotient 变量的值为：%d %s\n", quotient, strerror(errno));
+
 	system("pause");
+	exit(EXIT_SUCCESS);
 }
