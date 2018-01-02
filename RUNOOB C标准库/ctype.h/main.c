99f2bf5f96ccabd65d4068d2611e84c22c389766
Gaolbirdy <gaolbird@yeah.net>
2018-01-02 22:31:33 +0800

    commit

--- RUNOOB C标准库/ctype.h/main.c
+++ RUNOOB C标准库/ctype.h/main.c
@@ -141,3 +141,247 @@
 //
 //	getch();
 //}
+
+//void main()
+//{
+//	int var1 = '3';
+//	int var2 = 'm';
+//	int var3 = ' ';
+//
+//	if (isgraph(var1))
+//	{
+//		printf("var1 = |%c| 是可打印的\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 是不可打印的\n", var1);
+//	}
+//
+//	if (isgraph(var2))
+//	{
+//		printf("var2 = |%c| 是可打印的\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 是不可打印的\n", var2);
+//	}
+//
+//	if (isgraph(var3))
+//	{
+//		printf("var3 = |%c| 是可打印的\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 是不可打印的\n", var3);
+//	}
+//
+//	getch();
+//}
+
+//void main()
+//{
+//	int var1 = 'Q';
+//	int var2 = 'q';
+//	int var3 = '3';
+//
+//	if (islower(var1))
+//	{
+//		printf("var1 = |%c| 是小写字母\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 不是小写字母\n", var1);
+//	}
+//
+//	if (islower(var2))
+//	{
+//		printf("var2 = |%c| 是小写字母\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 不是小写字母\n", var2);
+//	}
+//
+//	if (islower(var3))
+//	{
+//		printf("var3 = |%c| 是小写字母\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 不是小写字母\n", var3);
+//	}
+//
+//	getch();
+//}
+
+//void main()
+//{
+//	int var1 = 'k';
+//	int var2 = '8';
+//	int var3 = '\t';
+//	int var4 = ' ';
+//
+//	if (isprint(var1))
+//	{
+//		printf("var1 = |%c| 是可打印的\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 是不可打印的\n", var1);
+//	}
+//
+//	if (isprint(var2))
+//	{
+//		printf("var2 = |%c| 是可打印的\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 是不可打印的\n", var2);
+//	}
+//
+//	if (isprint(var3))
+//	{
+//		printf("var3 = |%c| 是可打印的\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 是不可打印的\n", var3);
+//	}
+//
+//	if (isprint(var4))
+//	{
+//		printf("var4 = |%c| 是可打印的\n", var4);
+//	}
+//	else
+//	{
+//		printf("var4 = |%c| 是不可打印的\n", var4);
+//	}
+//
+//	getch();
+//}
+
+//void main()
+//{
+//	int var1 = 't';
+//	int var2 = '1';
+//	int var3 = '/';
+//	int var4 = ' ';
+//
+//	if (ispunct(var1))
+//	{
+//		printf("var1 = |%c| 是标点符号字符\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 不是标点符号字符\n", var1);
+//	}
+//
+//	if (ispunct(var2))
+//	{
+//		printf("var2 = |%c| 是标点符号字符\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 不是标点符号字符\n", var2);
+//	}
+//
+//	if (ispunct(var3))
+//	{
+//		printf("var3 = |%c| 是标点符号字符\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 不是标点符号字符\n", var3);
+//	}
+//
+//	if (ispunct(var4))
+//	{
+//		printf("var4 = |%c| 是标点符号字符\n", var4);
+//	}
+//	else
+//	{
+//		printf("var4 = |%c| 不是标点符号字符\n", var4);
+//	}
+//
+//	getch();
+//}
+
+//void main()
+//{
+//	int var1 = 't';
+//	int var2 = '1';
+//	int var3 = ' ';
+//
+//	if (isspace(var1))
+//	{
+//		printf("var1 = |%c| 是空白字符\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 不是空白字符\n", var1);
+//	}
+//
+//	if (isspace(var2))
+//	{
+//		printf("var2 = |%c| 是空白字符\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 不是空白字符\n", var2);
+//	}
+//
+//	if (isspace(var3))
+//	{
+//		printf("var3 = |%c| 是空白字符\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 不是空白字符\n", var3);
+//	}
+//
+//	getch();
+//}
+
+//void main()
+//{
+//	int var1 = 'M';
+//	int var2 = 'm';
+//	int var3 = '3';
+//
+//	if (isupper(var1))
+//	{
+//		printf("var1 = |%c| 是大写字母\n", var1);
+//	}
+//	else
+//	{
+//		printf("var1 = |%c| 不是大写字母\n", var1);
+//	}
+//
+//	if (isupper(var2))
+//	{
+//		printf("var2 = |%c| 是大写字母\n", var2);
+//	}
+//	else
+//	{
+//		printf("var2 = |%c| 不是大写字母\n", var2);
+//	}
+//
+//	if (isupper(var3))
+//	{
+//		printf("var3 = |%c| 是大写字母\n", var3);
+//	}
+//	else
+//	{
+//		printf("var3 = |%c| 不是大写字母\n", var3);
+//	}
+//
+//	getch();
+//}
+
+void main()
+{
+	char var1 = "tuts";
+	char var2 = "0xE";
+
+	getch();
+}
