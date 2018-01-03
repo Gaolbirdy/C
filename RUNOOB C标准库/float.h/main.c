68c38d39aacbb79e93cec079152241e3af60c5c0
Gaolbirdy <gaolbird@yeah.net>
2018-01-03 23:00:20 +0800

    commit

--- RUNOOB C标准库/float.h/main.c
+++ RUNOOB C标准库/float.h/main.c
@@ -4,6 +4,10 @@
 
 void main()
 {
+	printf("The maximum value of float = %.10e\n", FLT_MAX);
+	printf("The minimum value of float = %.10e\n", FLT_MIN);
+
+	printf("The number of digits in the number  = %.10e\n", FLT_MANT_DIG);
 
 	getch();
 }
---
+++ RUNOOB C标准库/locale.h/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <time.h>
+
+//void main()
+//{
+//	time_t curtime;
+//	struct tm *timer;
+//	char buffer[80];
+//
+//	time(&curtime);
+//	timer = localtime(&curtime);
+//
+//	printf("Locale is : %s\n", setlocale(LC_ALL, "en_GB"));
+//	strftime(buffer, 80, "%c", timer);
+//	printf("Date is : %s\n", buffer);
+//
+//	printf("Locale is : %s\n", setlocale(LC_ALL, "de_DE"));
+//	strftime(buffer, 80, "%c", timer);
+//	printf("Date is : %s\n", buffer);
+//
+//	getch();
+//}
+
+void main()
+{
+
+	struct lconv
+	getch();
+}
