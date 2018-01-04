d62cc4d98d60ff493b4a32fdcc374e773e6f9879
Gaolbirdy <gaolbird@yeah.net>
2018-01-04 22:41:39 +0800

    commit

--- RUNOOB C标准库/signal.h/main.c
+++ RUNOOB C标准库/signal.h/main.c
@@ -8,19 +8,18 @@ void sighandler(int);
 
 void signal_catchfunc(int);
 
-
-void main()
-{
-	signal(SIGINT, sighandler);
-
-	while (1)
-	{
-		printf("开始休眠一秒钟...\n");
-		Sleep(1000);
-	}
-
-	//getch();
-}
+//void main()
+//{
+//	signal(SIGINT, sighandler);
+//
+//	while (1)
+//	{
+//		printf("开始休眠一秒钟...\n");
+//		Sleep(1000);
+//	}
+//
+//	//getch();
+//}
 
 void sighandler(int signum)
 {
@@ -28,28 +27,29 @@ void sighandler(int signum)
 	//exit(1);
 }
 
-//void main()
-//{
-//	int ret;
-//
-//	ret = signal(SIGINT, signal_catchfunc);
-//
-//	if (ret == SIG_ERR)
-//	{
-//		printf("错误：不能设置信号处理程序。\n");
-//		exit(0);
-//	}
-//	printf("开始生成一个信号\n");
-//	ret = raise(SIGINT);
-//	if (ret != 0)
-//	{
-//		printf("错误：不能生成 SIGINT 信号。\n");
-//		exit(0);
-//	}
-//	printf("退出...\n");
-//
-//	getch();
-//}
+void main()
+{
+	int ret;
+
+	ret = signal(SIGINT, signal_catchfunc);
+	printf("%d", ret);
+
+	if (ret == SIG_ERR)
+	{
+		printf("错误：不能设置信号处理程序。\n");
+		exit(0);
+	}
+	printf("开始生成一个信号\n");
+	ret = raise(SIGINT);
+	if (ret != 0)
+	{
+		printf("错误：不能生成 SIGINT 信号。\n");
+		exit(0);
+	}
+	printf("退出...\n");
+
+	getch();
+}
 
 void signal_catchfunc(int signal)
 {
